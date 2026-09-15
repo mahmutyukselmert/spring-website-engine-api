32e1514d1668b851d8009e39eab616a8b83bdf09
Mahmut Yüksel Mert <mahmutyukselmert@gmail.com>
2026-09-15 23:23:14 +0300

    first commit

---
+++ src/main/java/com/mym/spring_website_engine_api/SpringWebsiteEngineApiApplication.java
@@ -0,0 +1,13 @@
+package com.mym.spring_website_engine_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebsiteEngineApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebsiteEngineApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mym/spring_website_engine_api/SpringWebsiteEngineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mym.spring_website_engine_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebsiteEngineApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
